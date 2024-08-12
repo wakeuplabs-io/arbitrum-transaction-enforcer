17a642da35afa2864437723a5662d3b0f6b7cf13
Facundo De Lorenzo <fdelorenzo.dev@gmail.com>
2024-08-12 17:47:08 -0300

    arbitrum connect home redirect

--- packages/ui/src/components/layout/topbar.tsx
+++ packages/ui/src/components/layout/topbar.tsx
@@ -1,13 +1,14 @@
 import ArbitrumConnectIcon from "@/assets/arbitrum-connect.svg";
 import CustomConnectButton from "@/components/connect-wallet";
-import { Link } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
 import { WalletIcon } from "lucide-react";
 
 export default function Topbar() {
+  const navigate = useNavigate();
   return (
-    <header className="flex justify-between items-center | sticky top-0 sm:px-8 | h-[4rem] card--blur z-10 bg-white px-4">
+    <header className="flex justify-between items-center | sticky top-0 sm:px-8 | h-[6rem] card--blur z-10 bg-white px-4">
       <div className="flex justify-between items-center | w-full">
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-3 cursor-pointer" onClick={() => navigate({ to: "/" })}>
           <img src={ArbitrumConnectIcon} width={32} height={32} />
           <span className="text-2xl hidden md:block">
             <b>Arbitrum</b> Connect
